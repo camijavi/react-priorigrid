12ca6a546ececa50a8b5931c5f1919bca56639f3
camijavi <camijavinavarro@gmail.com>
2026-09-15 11:24:00 -0600

    Add newEditTaskDialog and taskCard components

---
+++ src/components/newEditTaskDialog.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const newEditTaskDialog = () => {
+  return (
+    <div>newEditTaskDialog</div>
+  )
+}
+
+export default newEditTaskDialog
---
+++ src/components/taskCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const taskCard = () => {
+  return (
+    <div>
+        <h1>Title</h1>
+    </div>
+  )
+}
+
+export default taskCard
