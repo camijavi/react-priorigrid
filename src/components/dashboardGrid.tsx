6994151866dda95d6e6bf14c646914c821b423bb
camijavi <camijavinavarro@gmail.com>
2026-09-15 19:28:57 -0600

    add dashboard grid and quadrant detail components and screen

---
+++ src/components/dashboardGrid.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const dashboardGrid = () => {
+  return (
+    <div>dashboardGrid</div>
+  )
+}
+
+export default dashboardGrid
---
+++ src/pages/quadrantDetailScren.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const quadrantDetailScren = () => {
+  return (
+    <div>quadrantDetailScren</div>
+  )
+}
+
+export default quadrantDetailScren
